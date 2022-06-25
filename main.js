c026d299bcf55e2f9783401ad86d9148ccff7fd3
cosmic-pooh <65557174+cosmic-pooh@users.noreply.github.com>
2022-06-25 16:07:07 +0900

    6/25更新⑥
    フォントサイズ調整

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
 $(function(){
-// 写真を左からフェードインさせる
-  $(".inview_re").on("inview", function (event, isInView) {
-    if (isInView) {
-      $(this).stop().addClass("is-show");
-    } else {
-      $(this).stop().removeClass("is-show");
-    }
-  });
-// 写真を右からフェードインさせる 
   $(".inview_re").on("inview", function (event, isInView) {
     if (isInView) {
       $(this).stop().addClass("is-show");
@@ -16,4 +7,3 @@ $(function(){
     }
   });
 });
-
